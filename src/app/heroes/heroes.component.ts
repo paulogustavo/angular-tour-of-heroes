a5bd6e53f8050c9c95ed67f05c9d5c1cff90e39e
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2023-06-27 08:44:26 -0300

    feat: service added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
-import { HEROES } from './../mock-heroes';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -11,11 +11,12 @@ export class HeroesComponent implements OnInit {
 
   selectedHero?: Hero;
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
   onSelect(hero: Hero){
