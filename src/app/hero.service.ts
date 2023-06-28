beda6a2bb0e0859ba9a7345a01169ce275051685
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2023-06-27 23:30:22 -0300

    feat: fetching heroes via httpClient get

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
@@ -9,17 +10,26 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';
+
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient
+  ) { }
 
   getHeroes(): Observable<Hero[]>{
-    const heroes =  of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
-    return heroes;
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
+
   }
 
   getHero(id: number): Observable<Hero>{
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`fetched hero id=${id}`);
     return of(hero);
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
